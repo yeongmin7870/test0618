9e4c33bb32f05acb62e8a9a3e80782093c463cc5
희원 김 <khw7390@naver.com>
2024-06-18 16:32:22 +0900

    계산기 만들기 라벨 계산화면, 버튼으로 계산기 버튼들 구현함

--- test0618/test0618/Form1.Designer.vb
+++ test0618/test0618/Form1.Designer.vb
@@ -22,51 +22,341 @@ Partial Class Form1
     '코드 편집기에서는 수정하지 마세요.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'Label1
         '
-        Me.TextBox1.BackColor = System.Drawing.Color.Black
-        Me.TextBox1.Font = New System.Drawing.Font("굴림", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.TextBox1.ForeColor = System.Drawing.Color.White
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(361, 317)
-        Me.TextBox1.TabIndex = 0
-        Me.TextBox1.Text = "입력해보시든가."
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(0, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(428, 40)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "0"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 4
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Button16, 3, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button15, 2, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button14, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button13, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button12, 3, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button11, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button9, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button8, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button7, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button6, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button5, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button4, 3, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button3, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button2, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button10, 1, 2)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(7, 43)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(424, 305)
+        Me.TableLayoutPanel1.TabIndex = 3
         '
         'Button1
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.Control
-        Me.Button1.Font = New System.Drawing.Font("굴림", 12.0!, System.Drawing.FontStyle.Bold)
-        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Button1.Location = New System.Drawing.Point(379, 12)
+        Me.Button1.BackColor = System.Drawing.Color.DarkGray
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Snow
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(120, 48)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "딸깍"
+        Me.Button1.Size = New System.Drawing.Size(100, 70)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "7"
         Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkGray
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button2.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Snow
+        Me.Button2.Location = New System.Drawing.Point(109, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 70)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "8"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DarkGray
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button3.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Snow
+        Me.Button3.Location = New System.Drawing.Point(215, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 70)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "9"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.DarkGray
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button4.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.Snow
+        Me.Button4.Location = New System.Drawing.Point(321, 3)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 70)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "÷"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.DarkGray
+        Me.Button5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button5.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Snow
+        Me.Button5.Location = New System.Drawing.Point(3, 79)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 70)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "4"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.DarkGray
+        Me.Button6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button6.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button6.ForeColor = System.Drawing.Color.Snow
+        Me.Button6.Location = New System.Drawing.Point(109, 79)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 70)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.DarkGray
+        Me.Button7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button7.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button7.ForeColor = System.Drawing.Color.Snow
+        Me.Button7.Location = New System.Drawing.Point(215, 79)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 70)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.DarkGray
+        Me.Button8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button8.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button8.ForeColor = System.Drawing.Color.Snow
+        Me.Button8.Location = New System.Drawing.Point(321, 79)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 70)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "×"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.DarkGray
+        Me.Button9.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button9.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button9.ForeColor = System.Drawing.Color.Snow
+        Me.Button9.Location = New System.Drawing.Point(3, 155)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 70)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "1"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.DarkGray
+        Me.Button10.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button10.ForeColor = System.Drawing.Color.Snow
+        Me.Button10.Location = New System.Drawing.Point(109, 155)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(100, 70)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "2"
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.DarkGray
+        Me.Button11.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button11.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button11.ForeColor = System.Drawing.Color.Snow
+        Me.Button11.Location = New System.Drawing.Point(215, 155)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(100, 70)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "3"
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.DarkGray
+        Me.Button12.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button12.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button12.ForeColor = System.Drawing.Color.Snow
+        Me.Button12.Location = New System.Drawing.Point(321, 155)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(100, 70)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "－"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.Color.DarkGray
+        Me.Button13.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button13.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button13.ForeColor = System.Drawing.Color.Snow
+        Me.Button13.Location = New System.Drawing.Point(3, 231)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(100, 71)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "AC"
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.DarkGray
+        Me.Button14.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button14.ForeColor = System.Drawing.Color.Snow
+        Me.Button14.Location = New System.Drawing.Point(109, 231)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(100, 70)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "0"
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.DarkGray
+        Me.Button15.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button15.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button15.ForeColor = System.Drawing.Color.Snow
+        Me.Button15.Location = New System.Drawing.Point(215, 231)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(100, 71)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = "."
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.DarkGray
+        Me.Button16.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button16.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button16.ForeColor = System.Drawing.Color.Snow
+        Me.Button16.Location = New System.Drawing.Point(321, 231)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(100, 71)
+        Me.Button16.TabIndex = 15
+        Me.Button16.Text = "＋"
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button17
+        '
+        Me.Button17.BackColor = System.Drawing.Color.DarkGray
+        Me.Button17.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button17.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Bold)
+        Me.Button17.ForeColor = System.Drawing.Color.Snow
+        Me.Button17.Location = New System.Drawing.Point(0, 348)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(428, 52)
+        Me.Button17.TabIndex = 4
+        Me.Button17.Text = "="
+        Me.Button17.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(502, 341)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(428, 400)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
         Me.ForeColor = System.Drawing.SystemColors.Control
         Me.Name = "Form1"
         Me.Text = "영만이와 첫경험"
         Me.TransparencyKey = System.Drawing.Color.White
+        Me.TableLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button17 As Button
 End Class
--- test0618/test0618/Form1.vb
+++ test0618/test0618/Form1.vb
@@ -1,9 +1,24 @@
-﻿Public Class Form1
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+﻿
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+        Dim dlls = New dllTest.Class1()
+        Dim test = dlls.add("코딩 어렵다.")
+        Dim Message = String.Empty
+        Message = String.Format(TextBox1.Text)
+        MessageBox.Show(Message)
 
     End Sub
+
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
 End Class
